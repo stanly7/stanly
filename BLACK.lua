e7b0ca2a29325605d22128afad23bc636d108ea4
BLACKBOTSS <64044813+BLACKBOTSS@users.noreply.github.com>
2020-04-29 04:44:42 +0300

    Add files via upload

--- BLACK.lua
+++ BLACK.lua
@@ -6486,6 +6486,11 @@ if #list == 0 then
 send(msg.chat_id_, msg.id_,'⚠️┇ لا توجد صلاحيات مضافه')
 return false
 end
+if text:match('^الحساب (%d+)$') then
+local id = text:match('^الحساب (%d+)$')
+local text = 'اضغط لمشاهده العضو 🚸'
+tdcli_function ({ID="SendMessage", chat_id_=msg.chat_id_, reply_to_message_id_=msg.id_, disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=text, disable_web_page_preview_=1, clear_draft_=0, entities_={[0] = {ID="MessageEntityMentionName", offset_=0, length_=19, user_id_=id}}}}, dl_cb, nil)
+end
 t = "\n☑️┇ قائمة الصلاحيات المضافه \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n"
 for k,v in pairs(list) do
 var = database:get(bot_id.."Comd:New:rt:bot:"..v..msg.chat_id_)
