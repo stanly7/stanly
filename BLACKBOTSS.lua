59cdc2ab610ec3022055226cdb42b22a740caed3
BLACKBOTSS <64044813+BLACKBOTSS@users.noreply.github.com>
2020-07-25 01:38:54 +0300

    Add files via upload

--- BLACKBOTSS.lua
+++ BLACKBOTSS.lua
@@ -4075,30 +4075,30 @@ end
 end
 if text == "تفعيل جلب الرابط" or text == 'تفعيل الرابط' then
 if Addictive(msg) then  
-database:set(bot_id.."Group:Link"..msg.chat_id_,true) 
-send(msg.chat_id_, msg.id_,"☑⌔︙تم تفعيل الرابط") 
+database:set(bot_id.."BLACKBOTSS:Link_Group"..msg.chat_id_,true) 
+send(msg.chat_id_, msg.id_,"⌔︙تم تفعيل الرابط") 
 return false  
 end
 end
 if text == "تعطيل جلب الرابط" or text == 'تعطيل الرابط' then
 if Addictive(msg) then  
-database:del(bot_id.."Group:Link"..msg.chat_id_) 
+database:del(bot_id.."BLACKBOTSS:Link_Group"..msg.chat_id_) 
 send(msg.chat_id_, msg.id_,"⌔︙تم تعطيل الرابط") 
 return false end
 end
 if text == "الرابط" or text == "رابط الكروب" then 
-local status_Link = database:get(bot_id.."Group:Link"..msg.chat_id_)
+local status_Link = database:get(bot_id.."BLACKBOTSS:Link_Group"..msg.chat_id_)
 if not status_Link then
 send(msg.chat_id_, msg.id_,"⌔︙المدراء قاموا بتعطيل الرابط") 
 return false  
 end
-local link = database:get(bot_id.."Private:Group:Link"..msg.chat_id_)            
+local link = database:get(bot_id.."BLACKBOTSS:Private:Group:Link"..msg.chat_id_)            
 if link then                              
-send(msg.chat_id_,msg.id_,'⌔︙*رابط المجموعة* -\n ['..link..']')                          
+send(msg.chat_id_,msg.id_,'⌔︙*Link group* -\n ['..link..']')                          
 else                
 local linkgpp = json:decode(https.request('https://api.telegram.org/bot'..token..'/exportChatInviteLink?chat_id='..msg.chat_id_))
 if linkgpp.ok == true then 
-linkgp = '⌔︙ *رابط المجموعة* - \n ['..linkgpp.result..']'
+linkgp = '⌔︙ *Link group* - \n ['..linkgpp.result..']'
 else
 linkgp = '⌔︙ لا يوجد رابط ارسل ضع رابط'
 end  
@@ -4108,7 +4108,7 @@ end
 if text == 'مسح الرابط' or text == 'حذف الرابط' then
 if Addictive(msg) then
 send(msg.chat_id_,msg.id_,"⌔︙ تم مسح الرابط ")           
-database:del(bot_id.."Private:Group:Link"..msg.chat_id_) 
+database:del(bot_id.."BLACKBOTSS:Private:Group:Link"..msg.chat_id_) 
 return false      
 end
 end
